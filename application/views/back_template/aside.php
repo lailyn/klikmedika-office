a6015feeaefd3f2681c9911866d0de683fabdd99
Ahmad Husna Ahadi <husna3305@gmail.com>
2025-09-17 08:52:56 +0700

    payouts

--- application/views/back_template/aside.php
+++ application/views/back_template/aside.php
@@ -380,12 +380,6 @@
                       <p>Payouts</p>
                     </a>
                   </li>
-                  <li class="nav-item">
-                    <a href="<?= site_url('dwigital/transaksi/payouts/infografis') ?>" class="nav-link">
-                      <i class="far fa-dot-circle nav-icon"></i>
-                      <p>Payouts Infografis</p>
-                    </a>
-                  </li>
                 </ul>
               </li>
             </ul>
